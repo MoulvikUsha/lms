988a28c1a7ebe3aaafad3e88d92b12b65e49ff37
MoulvikUsha <naanumoulvik@gmail.com>
2022-06-22 12:29:48 +0530

    dashboard

--- src/app/mentor/dashboard/dashboard.component.ts
+++ src/app/mentor/dashboard/dashboard.component.ts
@@ -12,6 +12,7 @@ export class DashboardComponent implements OnInit {
   myChart1: any;
   myChart2: any;
   myChart3: any;
+  myChart4: any;
 
   constructor() { }
 
@@ -39,6 +40,8 @@ export class DashboardComponent implements OnInit {
         }]
       },
       options: {
+        // responsive: false,
+        maintainAspectRatio: false,
         cutout: '75%',
         radius: 80,
         plugins: {
@@ -76,7 +79,8 @@ export class DashboardComponent implements OnInit {
               '#02C5E9','#02C5E9','#02C5E9','#02C5E9','#02C5E9','#02C5E9','#02C5E9',
             ],
             borderWidth: 0,
-            barThickness: 12
+            barThickness: 10,
+            borderRadius: 4
           },
         ],
       },
@@ -134,7 +138,8 @@ export class DashboardComponent implements OnInit {
               '#02C5E9',
             ],
             borderWidth: 0,
-            barThickness: 12
+            barThickness: 10,
+            borderRadius: 4,
           },
         ],
       },
@@ -169,7 +174,67 @@ export class DashboardComponent implements OnInit {
       },
     });
 
-    this.myChart3 = new Chart('performance', {
+    this.myChart3 = new Chart('degree', {
+      type: 'bar',
+      data: {
+        labels: [
+          'BE (CSE)',
+          'BE (Non-CSE)',
+          'Post Graduates',
+          'PhD',
+        ],
+        datasets: [
+          {
+            label: '',
+            // data: this.barChartData,
+            data: [15, 19, 8, 2],
+            backgroundColor: [
+              '#FF007C',
+              '#FF007C',
+              '#FF007C',
+              '#FF007C',
+            ],
+            borderWidth: 0,
+            barThickness: 15,
+            borderRadius: 4,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: 'right',
+            display: false,
+            labels: {
+              boxWidth: 15,
+              usePointStyle: true,
+            },
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: true,
+              drawBorder: false,
+              offset: false,
+              tickWidth: 0,
+              color: "#BCBCCB",
+            },
+          },
+          y: {
+            display: false,
+            grid: {
+              display: false,
+              drawBorder: false,
+            },
+          },
+        },
+      },
+    });
+
+    this.myChart4 = new Chart('performance', {
       type: 'pie',
       data: {
         labels: [
@@ -186,31 +251,22 @@ export class DashboardComponent implements OnInit {
             data: [65, 59, 80, 81, 56],
             backgroundColor: ['#39BB5C','#2DB5EE','#E4D402','#EA8604','#E40347'],
             borderWidth: 0,
+            hoverOffset: 15,
           },
         ],
       },
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
         plugins: {
           legend: {
             position: 'right',
             display: true,
             labels: {
-              boxWidth: 200,
+              boxWidth: 9,
               pointStyle: 'circle',
               usePointStyle: true,
             }
-          },
-        },
-        scales: {
-          x: {
-            display: false,
-            grid: {
-              display: false,
-              drawBorder: false,
-            },
-          },
-          y: {
-            display: false,
           },
         },
       },
