3eb60e700073c782c9d60bd0a18731f1e2729313
MoulvikUsha <naanumoulvik@gmail.com>
2022-06-30 18:51:39 +0530

    mock-rating

--- src/app/mentor/mock-rating/mock-rating.module.ts
+++ src/app/mentor/mock-rating/mock-rating.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MockRatingRoutingModule } from './mock-rating-routing.module';
+import { ShareModule } from 'src/app/share/share/share.module';
+import { MockRatingComponent } from './mock-rating.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MockRatingComponent
+  ],
   imports: [
     CommonModule,
-    MockRatingRoutingModule
+    MockRatingRoutingModule,
+    ShareModule
   ]
 })
 export class MockRatingModule { }
